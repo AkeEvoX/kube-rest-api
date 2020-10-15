7444c6d9eac28c9b72921d92ca3f2635f7e30323
AkeEvoX <svargalok@gmail.com>
2020-10-15 09:15:09 +0700

    change package

--- src/main/java/com/ktb/dpe/common/api/RestApiApplication.java
+++ src/main/java/com/ktb/dpe/common/api/RestApiApplication.java
@@ -1,4 +1,4 @@
-package com.ktb.dpe.common.api;
+package com.conq.rest.common.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
